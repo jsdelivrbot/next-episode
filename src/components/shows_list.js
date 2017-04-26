2966d750e62410fcd9d02a88aedc0073cdbb1f4c
Tom Shushan <tomshudev@gmail.com>
2017-04-26 23:12:15 +0300

    First commit and start of search api

---
+++ src/components/shows_list.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ShowListItem from './show_list_item';
+
+const ShowsList = (props) => {
+    const showItems = props.shows.map((show) => {
+        return (<ShowListItem 
+                onShowSelect = {props.onShowSelect}
+                key={show.id} 
+                show={show} />);
+        });
+
+    return (
+        <ul className="col-md-4 col-md-offset-2 list-group" >
+            {showItems}
+        </ul>
+    );
+}
+
+export default ShowsList;
